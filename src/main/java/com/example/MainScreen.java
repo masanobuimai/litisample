c782236b88d8c43bab695cf2afdfa95945c11a5a
masanobu.imai <masanobu.imai@gmail.com>
2019-07-19 14:03:34 +0900

    ライフバーを付けた。

--- src/main/java/com/example/MainScreen.java
+++ src/main/java/com/example/MainScreen.java
@@ -7,6 +7,8 @@ import de.gurkenlabs.litiengine.graphics.RenderEngine;
 import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 public class MainScreen extends GameScreen implements IUpdateable {
   private Hud hud;
@@ -17,6 +19,11 @@ public class MainScreen extends GameScreen implements IUpdateable {
     super("main");
     hud = new Hud(0, 0);
     getComponents().add(hud);
+    Game.graphics().onEntityRendered(e -> {
+      if (e.getRenderedObject() instanceof Mob) {
+        renderLifeBar((Mob) e.getRenderedObject(), e.getGraphics());
+      }
+    });
   }
 
   @Override
@@ -33,6 +40,25 @@ public class MainScreen extends GameScreen implements IUpdateable {
   public void render(Graphics2D g) {
     super.render(g);
     RenderEngine.render(g, Game.world().environment().getEmitters());
+  }
+
+  public void renderLifeBar(Mob e, Graphics2D g) {
+    if (!e.isDead()) {
+      double hpRatio = e.getHitPoints().getCurrentValue().doubleValue() / e.getHitPoints().getMaxValue().doubleValue();
+      double healthBarMaxWidth = e.getWidth();
+      double healthBarWidth = healthBarMaxWidth * hpRatio;
+      double healthBarHeight = 2.0D;
+      double x = Game.world().camera()
+                     .getViewportDimensionCenter(e).getX() - healthBarWidth / 2.0D;
+      double y = Game.world().camera()
+                     .getViewportDimensionCenter(e).getY() - (e.getHeight() * 3.0F / 4.0F);
+      Point2D healthBarOrigin = new Point2D.Double(x, y);
+      Rectangle2D rect = new Rectangle2D.Double(healthBarOrigin.getX(), healthBarOrigin.getY(), healthBarWidth, healthBarHeight);
+      g.setColor(Color.BLACK);
+      g.fill(new Rectangle2D.Double(rect.getX() - 1, rect.getY() - 1, healthBarMaxWidth + 2, rect.getHeight() + 2));
+      g.setColor(hpRatio < 0.5 ? Color.RED : Color.GREEN);
+      g.fill(rect);
+    }
   }
 
   @Override
